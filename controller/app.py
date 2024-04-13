be0858504f8cea5f5e7775d83a2c431a348d467e
Max Simmonds <max@maxs-air.home>
2024-04-13 11:36:12 +0100

    take off timer working

--- controller/app.py
+++ controller/app.py
@@ -1,18 +1,35 @@
 import tkinter as tk
+import controller.gui_components as gc
 
 class ControllerApp:
     def __init__(self, master):
         self.master = master
+
+        self.screen_width = self.master.winfo_screenwidth()
+        self.screen_height = self.master.winfo_screenheight()
+
+        self.master.geometry(f"{self.screen_width}x{self.screen_height}")
+
         self.master.title("Drone Remote Controller")
         self.create_widgets()
 
     def create_widgets(self):
         # Create GUI widgets (buttons, sliders, etc.)
+
+        # Take of Timer
+        self.t_o_time = gc.flight_time(self.master)
+        print(f'screen width: {self.screen_width}\nscreen height: {self.screen_height}')
+        self.x = int(self.screen_width*0.85)
+        self.y = int(self.screen_height*0.75)
+        self.t_o_time.place(x=self.x, y=self.y)
+
+        # Take off button
         
         pass
 
 def main():
-    root = tk.Tk()
+    root = tk.Tk() # this is the main window
+
     app = ControllerApp(root)
     root.mainloop()
 
--- controller/gui_components.py
+++ controller/gui_components.py
@@ -1,9 +1,25 @@
-class TakeoffButton(tk.Button):
+import time
+import tkinter as tk
+
+class takeoff_button(tk.Button):
     def __init__(self, master, drone_controller):
         super().__init__(master, text="Take Off", command=drone_controller.take_off)
 
-class LandButton(tk.Button):
+class land_button(tk.Button):
     def __init__(self, master, drone_controller):
         super().__init__(master, text="Land", command=drone_controller.land)
 
-# Define other GUI components as needed
+class flight_time(tk.Label):
+    def __init__(self, master):
+        super().__init__(master, text="Current Flight Time: 00:00:00", font=("TKDefaultFont", 25))
+        self.start_time = time.time()  # Initialize start time with current time in seconds since the epoch
+        self.update_interval = 50  # Update interval in milliseconds (1 second)
+    
+    def update_time(self):
+        elapsed_seconds = int(time.time() - self.start_time)  # Calculate elapsed time in seconds
+        hours = elapsed_seconds // 3600
+        minutes = (elapsed_seconds % 3600) // 60
+        seconds = elapsed_seconds % 60
+        time_str = f"Current Flight Time\n{hours:02d}:{minutes:02d}:{seconds:02d}"
+        self.config(text=time_str)  # Update the label text with the formatted elapsed time
+        self.after(10, self.update_time)  # Schedule the update_time method to be called again after the specified interval
